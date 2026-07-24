d82e6a91e944c0a63c214851097f96c08817b269
CarmelToaster <allenconnor304@gmail.com>
2026-07-23 21:15:37 -0500

    Update uart_tx.v

--- uart_tx.v
+++ uart_tx.v
@@ -1,33 +1,37 @@
-module uart_tx (
+module uart_tx #(parameter MAX = 13'd10416)(
     input wire clk,
     input wire reset,
     input wire tx_start,
-    input wire [13:0] tx_data,
+    input wire [7:0] tx_data,
     output reg tx
 );
 
-    reg [13:0] shift_reg;
+    reg [9:0] shift_reg;
+    reg [13:0] baud;
 
     always @(posedge clk) begin
         if (reset) begin
-            shift_reg <= 14'b11111111111111;
-            tx <= 1'b1;  
+            shift_reg <= 10'b1111111111;
+            tx <= 1'b1;
+            baud <= 14'd0;
         end else begin
-            if (tx_start) begin
-                // Load frame:
-                //
-                // [stop][data][start]
-                //
-                // start = 0
-                // stop  = 1
-                //
-                shift_reg <= {1'b1, tx_data, 1'b0};
-            end else begin
-                // Output current LSB
+
+            if (baud == MAX) begin
+                baud <= 14'd0;
+
                 tx <= shift_reg[0];
-                shift_reg <= {1'b0, shift_reg[13:1]};
+                shift_reg <= {1'b1, shift_reg[9:1]};
+
+            end else begin
+                baud <= baud + 1'b1;
             end
+
+            if (tx_start) begin
+                shift_reg <= {1'b1, tx_data, 1'b0};
+            end
+
         end
     end
+
 endmodule
 
